b47dae53b063ff3b1f19a1825db78793d9105496
mitchbr <brownmit@oregonstate.edu>
2021-09-03 17:05:06 -0700

    adding basic packet funcionality

--- RdtWithCpp/packet.cpp
+++ RdtWithCpp/packet.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <string>
+#include <string.h>
 
 using namespace std;
 
@@ -9,18 +9,67 @@ class Packet {
         int ackNum;
         int checksum;
         string data;
+        string packet;
     
     string toString() {
-        // TODO: Format data into a single string
-        return data;
+
+        // Add the sequence number to the string
+        string seqNumStr = to_string(seqNum);
+        for (int i = 0; i < 3-seqNumStr.length() ; i++) {
+            packet += "0";
+        }
+        packet += to_string(seqNum);
+
+        // Add the acknowledgement number to the string
+        string ackNumStr = to_string(ackNum);
+        for (int i = 0; i < 3-ackNumStr.length() ; i++) {
+            packet += "0";
+        }
+        packet += to_string(ackNum);
+
+        // Add the data to the string
+        packet += data;
+
+        return packet;
     }
 
-    int verifyChecksum() {
+    void fromString(string rawData) {
+        // TODO: Get fromString working
+        string seqNumStr, ackNumStr;
+        for (int i = 0; i < rawData.length(); i++) {
+            if (i < 3)
+                seqNumStr += rawData[i];
+            else if (i >= 3 && i < 6)
+                ackNumStr += rawData[i];
+            else
+                data += rawData[i];
+        }
+        seqNum = stoi( seqNumStr );
+        ackNum = stoi( ackNumStr );
+    }
+
+    void calcChecksum() {
         // TODO: Calculate checksum of data
-        return 1;
+        for (int i = 0; i < data.length(); i++) {
+            checksum += (int)data[i];
+        }
     }
 
     int dataLen() {
         return data.length();
     }
 };
+
+int main() {
+    Packet x = Packet();
+    x.seqNum = 1;
+    x.ackNum = 2;
+    x.data = "hey!";
+    string y = x.toString();
+    cout << y << endl;
+    Packet z = Packet();
+    z.fromString( y );
+    cout << z.data << endl;
+    z.calcChecksum();
+    printf("%d\n", z.checksum);
+}
