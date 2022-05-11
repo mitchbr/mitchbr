b2458ce2ca91d25885c75898e06ca26cdcdd18ab
mitchbr <brownmit@oregonstate.edu>
2022-05-11 08:28:34 -0700

    Setting up revised new_recipe

---
+++ RecipeApi/FlutterInterface/recipe_api/lib/components/new_recipe_components/new_recipe_preview.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class NewRecipePreview extends StatefulWidget {
+  const NewRecipePreview({Key? key}) : super(key: key);
+
+  @override
+  State<NewRecipePreview> createState() => _NewRecipePreviewState();
+}
+
+class _NewRecipePreviewState extends State<NewRecipePreview> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
