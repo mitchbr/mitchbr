42f077e2b60cca7b48bd6477e786ee0fd17ba7c6
mitchbr <brownmit@oregonstate.edu>
2022-05-12 10:08:16 -0700

    Added edit_recipes

---
+++ RecipeApi/FlutterInterface/recipe_api/lib/components/edit_recipe.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class EditRecipe extends StatefulWidget {
+  const EditRecipe({Key? key}) : super(key: key);
+
+  @override
+  State<EditRecipe> createState() => _EditRecipeState();
+}
+
+class _EditRecipeState extends State<EditRecipe> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
